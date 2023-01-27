4abd2f03aa733392bd3f7a1cccb5098f3a31edf4
DevExpressExampleBot <fakeEmail>
2023-01-27 13:47:49 +0400

    autogenerate VB

---
+++ VB/ConditionalFormatting_WPF_Examples/BusinessObjects.vb
@@ -0,0 +1,41 @@
+Imports System
+Imports System.Collections.Generic
+Imports System.Windows.Controls
+Imports DevExpress.Spreadsheet
+
+Namespace ConditionalFormatting_WPF_Examples
+
+    Public Class Group
+
+        Public Property Header As String
+
+        Public Property Items As List(Of SpreadsheetExample)
+
+        Public Sub New(ByVal name As String)
+            Header = name
+            Items = New List(Of SpreadsheetExample)()
+        End Sub
+    End Class
+
+    Public Class SpreadsheetExample
+
+        Private _Action As Action(Of DevExpress.Spreadsheet.IWorkbook)
+
+        Public Property Header As String
+
+        Public Sub New(ByVal name As String, ByVal action As Action(Of IWorkbook))
+            Header = name
+            Me.Action = action
+        End Sub
+
+        Public Property Action As Action(Of IWorkbook)
+            Get
+                Return _Action
+            End Get
+
+            Private Set(ByVal value As Action(Of IWorkbook))
+                _Action = value
+            End Set
+        End Property
+    End Class
+End Namespace
---
+++ VB/ConditionalFormatting_WPF_Examples/MainWindow.xaml.vb
@@ -0,0 +1,51 @@
+Imports DevExpress.Spreadsheet
+Imports DevExpress.Xpf.NavBar
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Input
+
+Namespace ConditionalFormatting_WPF_Examples
+
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
+
+        Private workbook As IWorkbook
+
+        Public Sub New()
+            Me.InitializeComponent()
+            ' Access a workbook.
+            workbook = Me.spreadsheetControl1.Document
+            DataContext = Groups.InitData()
+        End Sub
+
+        Private Sub NavigationPaneView_MouseDoubleClick(ByVal sender As Object, ByVal e As MouseButtonEventArgs)
+            Dim item As NavBarItem = CType(sender, NavBarViewBase).GetNavBarItem(e)
+            If item IsNot Nothing Then
+                Dim example As SpreadsheetExample = TryCast(item.Content, SpreadsheetExample)
+                If example IsNot Nothing Then
+                    LoadDocumentFromFile()
+                    example.Action(workbook)
+                    SaveDocumentToFile()
+                End If
+            End If
+        End Sub
+
+        ' ------------------- Load and Save a Document -------------------
+        Private Sub LoadDocumentFromFile()
+'#Region "#LoadDocumentFromFile"
+            ' Load a workbook from a file.
+            workbook.LoadDocument("Documents\Document.xlsx", DocumentFormat.OpenXml)
+'#End Region  ' #LoadDocumentFromFile
+        End Sub
+
+        Private Sub SaveDocumentToFile()
+'#Region "#SaveDocumentToFile"
+            ' Save the modified document to a file.
+            workbook.SaveDocument("Documents\SavedDocument.xlsx", DocumentFormat.OpenXml)
+'#End Region  ' #SaveDocumentToFile
+        End Sub
+    End Class
+End Namespace
