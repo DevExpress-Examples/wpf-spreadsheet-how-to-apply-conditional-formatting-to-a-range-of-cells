a5b019d1a45c2849e1c245093dd56eb1a5a8cd15
DevExpressExampleBot <fakeEmail>
2023-01-27 13:49:31 +0400

    autogenerate VB

--- VB/ConditionalFormatting_WPF_Examples/BusinessObjects.vb
+++ VB/ConditionalFormatting_WPF_Examples/BusinessObjects.vb
@@ -1,45 +1,41 @@
-﻿Imports System
+Imports System
 Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
-Imports DevExpress.Xpf.NavBar
 Imports DevExpress.Spreadsheet
 
-
 Namespace ConditionalFormatting_WPF_Examples
-	Public Class Group
-		Public Property Header() As String
-		Public Property Items() As List(Of SpreadsheetExample)
 
-		Public Sub New(ByVal name As String)
-			Header = name
-			Items = New List(Of SpreadsheetExample)()
-		End Sub
-	End Class
+    Public Class Group
 
-	Public Class SpreadsheetExample
-		Public Property Header() As String
-		Public Sub New(ByVal name As String, ByVal action As Action(Of IWorkbook))
-			Header = name
-			Me.Action = action
-		End Sub
-		Private privateAction As Action(Of IWorkbook)
-		Public Property Action() As Action(Of IWorkbook)
-			Get
-				Return privateAction
-			End Get
-			Private Set(ByVal value As Action(Of IWorkbook))
-				privateAction = value
-			End Set
-		End Property
-	End Class
+        Public Property Header As String
+
+        Public Property Items As List(Of SpreadsheetExample)
+
+        Public Sub New(ByVal name As String)
+            Header = name
+            Items = New List(Of SpreadsheetExample)()
+        End Sub
+    End Class
+
+    Public Class SpreadsheetExample
+
+        Private _Action As Action(Of DevExpress.Spreadsheet.IWorkbook)
+
+        Public Property Header As String
+
+        Public Sub New(ByVal name As String, ByVal action As Action(Of IWorkbook))
+            Header = name
+            Me.Action = action
+        End Sub
+
+        Public Property Action As Action(Of IWorkbook)
+            Get
+                Return _Action
+            End Get
+
+            Private Set(ByVal value As Action(Of IWorkbook))
+                _Action = value
+            End Set
+        End Property
+    End Class
 End Namespace
--- VB/ConditionalFormatting_WPF_Examples/Groups.vb
+++ VB/ConditionalFormatting_WPF_Examples/Groups.vb
@@ -1,37 +1,31 @@
-﻿Imports System
 Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
 
 Namespace ConditionalFormatting_WPF_Examples
-	Partial Public Class Groups
-		Inherits List(Of Group)
 
-		Public Shared Function InitData() As Groups
-			Dim examples As New Groups()
+    Public Partial Class Groups
+        Inherits List(Of Group)
 
-'			#Region "GroupNodes"
-			examples.Add(New Group("Conditional Formatting Examples"))
-'			#End Region
-
-'			#Region "ExampleNodes"
-			' Add nodes to the "Conditional Formatting" group of examples.
-			examples(0).Items.Add(New SpreadsheetExample("Format values that are above or below average", ConditionalFormatting.AddAverageConditionalFormattingAction))
-			examples(0).Items.Add(New SpreadsheetExample("Format cells that are not between two specified values", ConditionalFormatting.AddRangeConditionalFormattingAction))
-			examples(0).Items.Add(New SpreadsheetExample("Format top ranked values", ConditionalFormatting.AddRankConditionalFormattingAction))
-			examples(0).Items.Add(New SpreadsheetExample("Format cells that contain the given text", ConditionalFormatting.AddTextConditionalFormattingAction))
-			examples(0).Items.Add(New SpreadsheetExample("Format only unique values", ConditionalFormatting.AddSpecialConditionalFormattingAction))
-			examples(0).Items.Add(New SpreadsheetExample("Format today's date", ConditionalFormatting.AddTimePeriodConditionalFormattingAction))
-			examples(0).Items.Add(New SpreadsheetExample("Format values that are greater than a specified value", ConditionalFormatting.AddExpressionConditionalFormattingAction))
-			examples(0).Items.Add(New SpreadsheetExample("Use a formula to determine which cells to format", ConditionalFormatting.AddFormulaExpressionConditionalFormattingAction))
-			examples(0).Items.Add(New SpreadsheetExample("Format cells by using a two-color scale", ConditionalFormatting.AddColorScale2ConditionalFormattingAction))
-			examples(0).Items.Add(New SpreadsheetExample("Format cells by using a three-color scale", ConditionalFormatting.AddColorScale3ConditionalFormattingAction))
-			examples(0).Items.Add(New SpreadsheetExample("Format cells by using data bars", ConditionalFormatting.AddDataBarConditionalFormattingAction))
-			examples(0).Items.Add(New SpreadsheetExample("Format cells by using a custom icon set", ConditionalFormatting.AddIconSetConditionalFormattingAction))
-
-			Return examples
-'			#End Region
-		End Function
-	End Class
+        Public Shared Function InitData() As Groups
+            Dim examples As Groups = New Groups()
+#Region "GroupNodes"
+            examples.Add(New Group("Conditional Formatting Examples"))
+#End Region
+#Region "ExampleNodes"
+            ' Add nodes to the "Conditional Formatting" group of examples.
+            examples(0).Items.Add(New SpreadsheetExample("Format values that are above or below average", ConditionalFormatting.AddAverageConditionalFormattingAction))
+            examples(0).Items.Add(New SpreadsheetExample("Format cells that are not between two specified values", ConditionalFormatting.AddRangeConditionalFormattingAction))
+            examples(0).Items.Add(New SpreadsheetExample("Format top ranked values", ConditionalFormatting.AddRankConditionalFormattingAction))
+            examples(0).Items.Add(New SpreadsheetExample("Format cells that contain the given text", ConditionalFormatting.AddTextConditionalFormattingAction))
+            examples(0).Items.Add(New SpreadsheetExample("Format only unique values", ConditionalFormatting.AddSpecialConditionalFormattingAction))
+            examples(0).Items.Add(New SpreadsheetExample("Format today's date", ConditionalFormatting.AddTimePeriodConditionalFormattingAction))
+            examples(0).Items.Add(New SpreadsheetExample("Format values that are greater than a specified value", ConditionalFormatting.AddExpressionConditionalFormattingAction))
+            examples(0).Items.Add(New SpreadsheetExample("Use a formula to determine which cells to format", ConditionalFormatting.AddFormulaExpressionConditionalFormattingAction))
+            examples(0).Items.Add(New SpreadsheetExample("Format cells by using a two-color scale", ConditionalFormatting.AddColorScale2ConditionalFormattingAction))
+            examples(0).Items.Add(New SpreadsheetExample("Format cells by using a three-color scale", ConditionalFormatting.AddColorScale3ConditionalFormattingAction))
+            examples(0).Items.Add(New SpreadsheetExample("Format cells by using data bars", ConditionalFormatting.AddDataBarConditionalFormattingAction))
+            examples(0).Items.Add(New SpreadsheetExample("Format cells by using a custom icon set", ConditionalFormatting.AddIconSetConditionalFormattingAction))
+            Return examples
+#End Region
+        End Function
+    End Class
 End Namespace
